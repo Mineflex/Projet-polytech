5bf6ac915b139ecd3fe1f7471386479162382b7f
nbrif <mineflex63360@gmail.com>
2026-01-21 18:57:26 +0100

    Initial commit

--- src/main/java/coreProtector/nnem/Main.java
+++ src/main/java/coreProtector/nnem/Main.java
@@ -1,7 +1,24 @@
 package coreProtector.nnem;
 
+import coreProtector.nnem.gameGestion.GamePanel;
+
+import javax.swing.*;
+
 public class Main {
 
+    public static  void  main(String[] args){
+        JFrame window= new JFrame();
+        // /!\ important pour que l'on puisse quitter le jeux, sinon on est bloqué...
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setTitle("Core Protector");
 
-    ^mplokijuhygfx
+        // on applique le panel CF GamePanel
+        GamePanel gamePanel= new GamePanel();
+        window.add(gamePanel);
+
+        window.pack(); // pour que le panel soit visible
+        window.setLocationRelativeTo(null); // pour que la fenetre soit au centre
+        window.setVisible(true);//pour que la fenetre soit visible
+
+    }
 }
---
+++ src/main/java/coreProtector/nnem/gameGestion/GamePanel.java
@@ -0,0 +1,28 @@
+package coreProtector.nnem.gameGestion;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+    //paramettres de l'ecran---------------
+
+    final int originalTileSize=32; // la taille de chacun de nos sprites et tiles (pixel art)
+    final int scale=3; // un multiplicateur pour augmenter la taille sur l'ecran car sinon tout est minuscule
+    final int tileSize= originalTileSize*scale; // La taille visible, donc agrandie, en pixel de chaque sprites
+
+    final int maxScreenCol=16; //Le nombre max de tiles en largeur sur l'ecran
+    final int maxSreenRow=12; // idem mais en longueurs
+    final int screenWidth=tileSize*maxScreenCol; // la taille final de l'ecran
+    final int screenHeight=tileSize*maxSreenRow;//    ^
+    //-------------------------------------
+
+
+    // creer un un panel qui va prendre toutes les info utiles de l'ecran pour y afficher
+    public GamePanel(){
+        this.setPreferredSize(new Dimension(screenWidth,screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+    }
+
+
+}

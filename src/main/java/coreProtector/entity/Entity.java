014852eeceacffc5f32322260f8bf5fa73e6f12a
Eliol43lol <issarteleliott43@gmail.com>
2026-04-28 15:18:26 +0200

    faute

--- src/main/java/coreProtector/entity/Entity.java
+++ src/main/java/coreProtector/entity/Entity.java
@@ -8,7 +8,7 @@ public class Entity {
     public  int worldx,worldy; //position des entitées en fonction de la map, et pas de l'ecran
     public int speed; //vitesse des entitées
     public int health;
-    public int strange;
+    public int strength;
 
 
     public BufferedImage up1, up2, up3,up4,down1,down2,down3,down4,left1,left2,left3,left4,right1,right2,right3,right4; //L'ensembles des sprites pour chaque animation de chaque entitées
